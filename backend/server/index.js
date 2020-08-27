0118b33a3b1c54379e1314fa797407fce95b00ba
SopanCristian <icsopanc@gmail.com>
2020-08-27 13:05:22 -0500

    líneas no colocadas en el curso

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+
+/////
+// en el curso no colocaron estas líneas
+/////
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("../config");
+/////
 const server = express();
 const cors = require("cors");
 const { Technology } = require("../models");
@@ -9,7 +14,11 @@ server.use(express.json());
 server.use(express.static(__dirname + "/../public"));
 server.use(cors());
 
+/////
+// en el curso no colocaron esta línea
+/////
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+/////
 
 //server.get("/", async(req,res)=>{
 server.get("/api/technologies", async (req, res) => {
@@ -19,7 +28,13 @@ server.get("/api/technologies", async (req, res) => {
     technology.logo = `${req.protocol}://${req.headers.host}/img/${technology.logo}`;
     return technology;
   });
+
+  /////
+  // en el curso no colocaron esta línea
+  /////
   mongoose.disconnect();
+  /////
+
   return res.send({ error: false, data: technologies });
   //return res.send("<h1>H1</h1>");
 });
